66558f88febc84a37d04b93c6ee1e997f651de17
Kevin Donnelly <kdonnelly08@gmail.com>
2018-05-14 21:24:11 -0500

    Consolidate lift tests to single well typed one.

--- tests/baselines/reference/higherKindedTypesLift2.js
+++ tests/baselines/reference/higherKindedTypesLift2.js
@@ -1,70 +1,4 @@
 //// [higherKindedTypesLift2.ts]
-export interface FMap<FA, FB> {
-    (fmapfa: FA): FB
-}
-
-interface Bounded<
-    BC<_BCT> extends Bound<BC, _BCT>,
-    Bound<
-        _BC<__BCT>,
-        _BT
-    >,
-    BT extends BTBound,
-    BTBound = BT
-> {}
-
-interface Functor<CX<_TX extends AXBound> extends Functor<CX, AX, AXBound>, AX extends AXBound, AXBound = AX> extends Bounded<CX, Functor, AX, AXBound> {
-    map<BX>(fmapx: (fmapxax: AX) => BX): CX<BX>;
-}
-
-interface FunctorFoo<AFoo> extends Functor<FunctorFoo, AFoo> {
-    map<BFoo>(fmapfoo: FMap<AFoo, BFoo>): FunctorFoo<BFoo>;
-    fooVal: AFoo
-}
-
-interface StaticFunctor<
-    SC<_T extends SCTBound> extends SCBound<SC, _T> & Bounded<SC, SCBound, _T, SCTBound>,
-    SCBound<
-     _BC<__BCT>,
-     _BT
-    >,
-    SCTBound = {}
-> {
-    <AS, BS>(fa2: SC<AS>, fmap2: FMap<AS,BS>): SC<BS>
-}
-
-
-declare function staticMap<C1<_T1 extends T1Bound> extends Functor<C1, T1Bound, T1Bound>, T1Bound, A1, B1>(fa1: C1<A1>, fmap1: FMap<A1, B1>): C1<B1>;
-
-
-interface LiftedResult<
-    LC<_LT extends LRTBound> extends LRCBound<LC, _LT> & Bounded<LC, LRCBound, _LT, LRTBound>,
-    LRCBound<
-    _BC<__BCT>,
-    _BT
-   >,
-   LRTBound 
-> {
-    <LA, LB>(lmap: FMap<LA,LB>): LiftedResult2<LC, LA, LB, LRCBound>
-}
-
-interface LiftedResult2<
-    LC2<_LT extends LA2 | LB2> extends LRC2Bound<LC2, _LT> & Bounded<LC2, LRC2Bound, _LT, LA2 | LB2>, 
-    LA2,
-    LB2,
-    LRC2Bound<
-        _BC<__BCT>,
-        _BT
-    >,
-    > extends FMap<LC2<LA2>, LC2<LB2>>{}
-
-declare function lift<
-    C<_T extends ActTBound> extends ActBound & Bounded<C, ActBound, _T, ActTBound>,
-    ActBound,
-    ActTBound
->
-(fToLift: StaticFunctor<C, ActBound, ActTBound>): LiftedResult<C, ActBound, ActTBound>
-
 interface StartVal {
     startVal: string
 }
@@ -73,21 +7,105 @@ interface EndVal {
     endVal: number
 }
 
+declare function testFunc(strarg: StartVal): EndVal
+
+export interface FMap<FA, FB> {
+    (fmapfa: FA): FB
+}
+
+interface Bounded<
+    BC<_BCT extends BTBound> extends Bound<BC, _BCT, BTBound>,
+    Bound<
+        _BC<__BCT>,
+        _BT,
+        _BTB
+    >,
+    BT extends BTBound,
+    BTBound
+> {}
+
+interface Functor<
+    CX<_TX extends _TXB, _TXB = {}> extends Functor<CX, _TX, _TXB>,
+    AX extends AXBound,
+    AXBound
+    > 
+        extends Bounded<CX, Functor, AX, AXBound> {
+    functorMap<BX extends AXBound>(fmapx: FMap<AX, BX>): CX<BX>;
+}
+
+interface StaticFunctor<
+    SC<_T extends _TB, _TB = {}> extends SCBound<SC, _T, _TB> & Bounded<SC, SCBound, _T, _TB>,
+    SCBound<
+     _BC<__BCT>,
+     _BT,
+     _BTB
+    >,
+    SCTBound = {}
+> {
+    <AS extends SCTBound, BS extends SCTBound>(fa2: SC<AS>, fmap2: FMap<AS,BS>): SC<BS>
+}
+
+
+declare function lift<
+    C<_T extends _TB, _TB = {}> extends ActBound<C, _T, _TB> & Bounded<C, ActBound, _T, _TB>,
+    ActBound<
+    _BC<__BCT>,
+    _BT,
+    _BTB
+   >,
+    ActTBound
+>
+(fToLift: StaticFunctor<C, ActBound, ActTBound>): LiftedResult<C, ActBound, ActTBound>
+
+
+interface LiftedResult<
+    LC<_LT extends _TB, _TB = {}> extends LRCBound<LC, _LT, _TB> & Bounded<LC, LRCBound, _LT, _TB>,
+    LRCBound<
+    _BC<__BCT>,
+    _BT,
+    _BTB
+   >,
+   LRTBound 
+> {
+    <LA, LB>(lmap: FMap<LA,LB>): LiftedResult2<LC, LA, LB, LRCBound>
+}
+
+interface LiftedResult2<
+    LC2<_LT extends LA2 | LB2> extends LRC2Bound<LC2, _LT,  LA2 | LB2> & Bounded<LC2, LRC2Bound, _LT, LA2 | LB2>, 
+    LA2,
+    LB2,
+    LRC2Bound<
+        _BC<__BCT>,
+        _BT,
+        _BTB
+    >,
+    > {
+        <FinalC<_FT extends LA2 | LB2> extends LC2<_FT>>(lc2la2: FinalC<LA2>): FinalC<LB2>
+    }
+
+declare function staticMap<C1<_T1 extends _T1B, _T1B = {}> extends Functor<C1, _T1, _T1B>, T1Bound, A1 extends T1Bound, B1 extends T1Bound>(fa1: C1<A1>, fmap1: FMap<A1, B1>): C1<B1>;
+
+
+interface FunctorFoo<AFoo> extends Functor<FunctorFoo, AFoo, {}> {
+    functorMap<BFoo>(fmapfoo: FMap<AFoo, BFoo>): FunctorFoo<BFoo>;
+    fooVal: AFoo
+}
 
 declare const fooObj: FunctorFoo<StartVal>
-declare function stringLength(strarg: StartVal): EndVal
+
+
 
 const liftedStaticMap = lift(staticMap);
-const liftedStringLength = liftedStaticMap(stringLength);
-const result = liftedStringLength(fooObj);
+const liftedTestFunc = liftedStaticMap(testFunc);
+const result = liftedTestFunc(fooObj);
 const expectedType: FunctorFoo<EndVal> = result;
-const expectError = liftedStringLength(result)
+const expectError = liftedTestFunc(result)
 
 
 declare const declaredStaticFunctor: StaticFunctor<Functor, Functor>
 const liftedDeclaredStaticFunctor = lift(declaredStaticFunctor);
-const liftedDeclaredStringLength = liftedDeclaredStaticFunctor(stringLength)
-const declaredResult = liftedDeclaredStringLength(fooObj);
+const liftedDeclaredTestFunc = liftedDeclaredStaticFunctor(testFunc)
+const declaredResult = liftedDeclaredTestFunc(fooObj);
 const expectedTypeDeclared: FunctorFoo<EndVal> = declaredResult;
 const expectErrorDeclared = liftedDeclaredStringLength(declaredResult)
 
@@ -96,12 +114,12 @@ const expectErrorDeclared = liftedDeclaredStringLength(declaredResult)
 "use strict";
 exports.__esModule = true;
 var liftedStaticMap = lift(staticMap);
-var liftedStringLength = liftedStaticMap(stringLength);
-var result = liftedStringLength(fooObj);
+var liftedTestFunc = liftedStaticMap(testFunc);
+var result = liftedTestFunc(fooObj);
 var expectedType = result;
-var expectError = liftedStringLength(result);
+var expectError = liftedTestFunc(result);
 var liftedDeclaredStaticFunctor = lift(declaredStaticFunctor);
-var liftedDeclaredStringLength = liftedDeclaredStaticFunctor(stringLength);
-var declaredResult = liftedDeclaredStringLength(fooObj);
+var liftedDeclaredTestFunc = liftedDeclaredStaticFunctor(testFunc);
+var declaredResult = liftedDeclaredTestFunc(fooObj);
 var expectedTypeDeclared = declaredResult;
 var expectErrorDeclared = liftedDeclaredStringLength(declaredResult);
